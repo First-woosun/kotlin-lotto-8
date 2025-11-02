f99db7835a883dcd8dd740a3044f28f3cf9dacf7
First_woosun <dbdntjs620@gmail.com>
2025-11-02 21:13:04 +0900

    feat :공백 입력 예외 추가
    - 공백이 입력될 시 IllegalArgumentException 발생

--- src/main/kotlin/lotto/inputBonusNumber.kt
+++ src/main/kotlin/lotto/inputBonusNumber.kt
@@ -4,5 +4,9 @@ import camp.nextstep.edu.missionutils.Console
 
 fun inputBonusNumber(): String{
     println("\n보너스 번호를 입력해주세요.")
-    return Console.readLine()
+    val returnString = Console.readLine()
+    if (returnString.isNullOrEmpty()) {
+        throw IllegalArgumentException("[ERROR] 값을 입력해주세요")
+    }
+    return returnString
 }
