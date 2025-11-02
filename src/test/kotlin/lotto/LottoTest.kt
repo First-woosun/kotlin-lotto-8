2f19269baa594d0e7740d6da5820af576a6cefb6
First_woosun <dbdntjs620@gmail.com>
2025-11-02 20:56:53 +0900

    feat : 중복 테스트 수정
    - 테스트를 통과하도록 프로덕션 수정

--- src/test/kotlin/lotto/LottoTest.kt
+++ src/test/kotlin/lotto/LottoTest.kt
@@ -15,7 +15,7 @@ class LottoTest {
     @Test
     fun `로또 번호에 중복된 숫자가 있으면 예외가 발생한다`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(listOf(1, 2, 3, 4, 5, 5))
+            winningNumbersDuplicateValidation(listOf(1, 2, 3, 4, 5, 5).toMutableList())
         }
     }
 
