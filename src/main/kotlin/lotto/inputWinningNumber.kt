099c9f160fb9a6d0d10135a53eb4b4cc72b8f85c
First_woosun <dbdntjs620@gmail.com>
2025-11-02 21:13:50 +0900

    feat : 당첨 번호 공백 입력 예외
    - 공백 입력시 IllegalArgumentException 발생

--- src/main/kotlin/lotto/inputWinningNumber.kt
+++ src/main/kotlin/lotto/inputWinningNumber.kt
@@ -5,7 +5,9 @@ import camp.nextstep.edu.missionutils.Console
 fun inputWinningNumber(): MutableList<String> {
     println("당첨 번호를 입력해 주세요.")
     val inputString = Console.readLine()
-
+    if (inputString.isNullOrEmpty()) {
+        throw IllegalArgumentException("[ERROR] 값을 입력해주세요.")
+    }
     val returnNumbers = inputString.split(",")
     return returnNumbers.toMutableList()
 }
