0a7acdac1ad1e2203113798df26b3c544ec7e85f
First_woosun <dbdntjs620@gmail.com>
2025-11-02 21:12:14 +0900

    feat : 공백 입력 예외 추가
    -공백 입력시 IllegalArgumentException 발생 추가

--- src/main/kotlin/lotto/costInputAndSave.kt
+++ src/main/kotlin/lotto/costInputAndSave.kt
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console
 
 fun costInputAndSave() : String {
     println("구입금액을 입력해 주세요.")
-    var input : String = Console.readLine()
+    val input = Console.readLine()
+    if(input.isNullOrEmpty()) {
+        throw IllegalArgumentException("[ERROR] 값을 입력해주세요.")
+    }
     return input
 }
